a2c212b639b35c122f3bddc69094480eaca3af9f
huongntt309 <huonguet8@gmail.com>
2024-04-24 21:51:51 +0700

    locust file

--- K6/script.js
+++ K6/script.js
@@ -3,21 +3,21 @@ import { sleep } from 'k6';
 
 // Options vus: virtual users, duration: total time 
 export const options = {
-    vus: 40000, // Virtual Users - VUs
-    duration: '100s', // Run the testing k6 file in duration time 
+    vus: 2000, // Virtual Users - VUs
+    duration: '50s', // Run the testing k6 file in duration time 
 };
 
 export default function () {
     let data = JSON.stringify({
-        "phone_number": "0123456787",
-        "password": "123456789"
+        "password": "admin",
+        "username": "admin"
     });
 
     let headers = {
         'Content-Type': 'application/json',
     };
 
-    let response = http.post('http://localhost:8081/api/v1/login', data, { headers: headers });
+    // let response = http.post('https://videogamedb.uk:443/api/authenticate', data, { headers: headers });
+    
     console.log(response.status);
-    sleep(1);
 }
--- locust/locustfile.py
+++ locust/locustfile.py
@@ -3,17 +3,20 @@ from locust import HttpUser, task, between
 
 class QuickstartUser(HttpUser):
     wait_time = between(1, 5)
+    
+    # host: https://videogamedb.uk:443
+    
+    @task
+    def get_video_game(self):
+        self.client.get("/api/videogame")
+        self.client.get("/api/videogame/1")
+        self.client.get("/api/videogame/2")
 
     @task
-    def hello_world(self):
-        self.client.get("/hello")
-        self.client.get("/world")
-
-    @task(3)
-    def view_items(self):
-        for item_id in range(10):
-            self.client.get(f"/item?id={item_id}", name="/item")
-            time.sleep(1)
-
-    def on_start(self):
-        self.client.post("/login", json={"username":"foo", "password":"bar"})
+    def authenticate(self):
+        payload = {
+            "password": "admin",
+            "username": "admin"
+        }
+        self.client.post("/api/authenticate", json=payload)
+        
